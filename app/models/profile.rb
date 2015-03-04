8833991a0b0f9f95a86f3443fa22d13a4c9900ee
Ashley Ellis <anellis12@gmail.com>
2015-03-04 14:36:51 -0500

    Add parse method

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,47 +1,56 @@
 class Profile < ActiveRecord::Base
   validates :username, uniqueness: true
-  before_create :cache
+  before_create :parse
 
   def initialize(username)
     @username= username
   end
 
-  def username
-    @content["login"]
-  end
-
-  def avatar_url
-    @content["avatar_url"]
-  end
-
-  def location
-    @content["location"]
-  end
-
-  def company_name
-    @content["company"]
-  end
-
-  def number_of_followers
-    @content["followers"]
-  end
-
-  def number_following
-    @content["following"]
-  end
-
-  private
-
-  def cache
-    if Profile.where(username: @username).blank?
-      @content = HTTParty.get(
+  def self.create_from_username(username)
+    @content = HTTParty.get(
       "https://api.github.com/users/#{username}",
       :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
       "User-Agent" => "anyone"
-    }
+      }
     )
+    if @content["login"]
+      Profile.create(body:@content)
     else
-      @content= Profile.find_by(username: @username).body
+      nil
+    end
   end
+  #
+  # def username
+  #   @content["login"]
+  # end
+  #
+  # def avatar_url
+  #   @content["avatar_url"]
+  # end
+  #
+  # def location
+  #   @content["location"]
+  # end
+  #
+  # def company_name
+  #   @content["company"]
+  # end
+  #
+  # def number_of_followers
+  #   @content["followers"]
+  # end
+  #
+  # def number_following
+  #   @content["following"]
+  # end
 
+  private
+    def parse
+      username = body["login"]
+      number_following = body["following"]
+      location = body["location"]
+      avatar_url = body["avatar_url"]
+      company_name = body["company"]
+      number_of_followers = body["followers"]
+    end
 end

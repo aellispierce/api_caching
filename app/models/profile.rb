3de7e79d4865f47bc837811bad01577f83fc06f5
Ashley Ellis <anellis12@gmail.com>
2015-03-04 14:58:12 -0500

    Add parse method

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,42 +7,42 @@ class Profile < ActiveRecord::Base
   end
 
   def self.create_from_username(username)
-    @content = HTTParty.get(
+    response = HTTParty.get(
       "https://api.github.com/users/#{username}",
       :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
       "User-Agent" => "anyone"
       }
     )
-    if @content["login"]
-      Profile.create(body:@content)
+    if response["login"]
+      Profile.create(body: response)
     else
       nil
     end
   end
-  #
-  # def username
-  #   @content["login"]
-  # end
-  #
-  # def avatar_url
-  #   @content["avatar_url"]
-  # end
-  #
-  # def location
-  #   @content["location"]
-  # end
-  #
-  # def company_name
-  #   @content["company"]
-  # end
-  #
-  # def number_of_followers
-  #   @content["followers"]
-  # end
-  #
-  # def number_following
-  #   @content["following"]
-  # end
+
+  def username
+    @content["login"]
+  end
+
+  def avatar_url
+    @content["avatar_url"]
+  end
+
+  def location
+    @content["location"]
+  end
+
+  def company_name
+    @content["company"]
+  end
+
+  def number_of_followers
+    @content["followers"]
+  end
+
+  def number_following
+    @content["following"]
+  end
 
   private
     def parse
